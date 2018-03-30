6774bce65474045298ecf113f4da0249d376e875
Daan Broekhof <daan.broekhof@gmail.com>
2018-03-31 01:37:54 +0200

    - Added links in type list

--- app/components/StructureTypeView.js
+++ app/components/StructureTypeView.js
@@ -169,7 +169,15 @@ class StructureTypeView extends Component {
 
           return <span><Link to={`/structure/${column.linkTo.replace('[self]', this.props.match.params.type)}/${linkToId}`}>{value}</Link></span>;
         };
+      } else {
+        _.filter(typeDefinition.relations, (x) => _.isEqual(x.path, c.dataIndex)).forEach(relation => {
+          c.renderer = ({value, column, row}) => {
+            const linkToId = column.linkKey ? _.get(row, column.linkKey) : value;
+            return <span><Link to={`/structure/${relation.toType}/${linkToId}`}>{value}</Link></span>;
+          };
+        });
       }
+
       return c;
     });
 
