eb2272a43f09faf3f5763d7cc522a3b2a5cca835
Daan Broekhof <daan.broekhof@gmail.com>
2018-03-30 14:23:44 +0200

    - Remove old code

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -4,12 +4,10 @@ import {createHashHistory} from 'history';
 import {routerMiddleware, routerActions} from 'react-router-redux';
 import {createLogger} from 'redux-logger';
 import rootReducer from '../reducers';
-import * as counterActions from '../actions/counter';
-import type {counterStateType} from '../reducers/counter';
 
 const history = createHashHistory();
 
-const configureStore = (initialState?: counterStateType) => {
+const configureStore = (initialState) => {
   // Redux Configuration
   const middleware = [];
   const enhancers = [];
@@ -34,7 +32,6 @@ const configureStore = (initialState?: counterStateType) => {
 
   // Redux DevTools Configuration
   const actionCreators = {
-    ...counterActions,
     ...routerActions,
   };
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
