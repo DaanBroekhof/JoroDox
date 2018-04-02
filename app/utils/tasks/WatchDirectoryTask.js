4811ea888a80be5433300f8ee4184c2e8aea5059
Daan Broekhof <daan.broekhof@gmail.com>
2018-04-02 16:40:23 +0200

    - Add file change watcher - Buffers filesystem changes into chunks for every 100 milliseconds

--- app/utils/tasks/WatchDirectoryTask.js
+++ app/utils/tasks/WatchDirectoryTask.js
@@ -1,6 +1,8 @@
 import BackgroundTask from './BackgroundTask';
 
 const fs = require('fs');
+const jetpack = require('fs-jetpack');
+const path = require('path');
 
 export default class WatchDirectoryTask extends BackgroundTask {
   static getTaskType() {
@@ -15,6 +17,7 @@ export default class WatchDirectoryTask extends BackgroundTask {
     if (request.type === 'close') {
       if (this.watcher) {
         this.watcher.close();
+        this.resolveFunc();
       }
     } else {
       super.handleRequest(request);
@@ -22,14 +25,52 @@ export default class WatchDirectoryTask extends BackgroundTask {
   }
 
   execute(args) {
-    this.watcher = fs.watch(args.rootDir, {recursive: true}, (eventType, filename) => {
+    let eventsBuffer = [];
+    this.lastEventTimestamp = 0;
+
+    this.watcher = fs.watch(args.rootDir, {recursive: true}, (rawEventType, filename) => {
       if (!filename) {
         return;
       }
 
-      if (eventType === 'rename' || eventType === 'change') {
-        this.sendResponse({eventType, filename});
+      const info = jetpack.inspect(path.join(args.rootDir, filename));
+
+      console.log({filename, rawEventType, info});
+
+      let eventType = rawEventType;
+      if (!info && rawEventType === 'rename') {
+        eventType = 'delete';
+      } else if (info && info.type === 'file' && rawEventType === 'change') {
+        eventType = 'fileChange';
+      } else if (info && info.type === 'file' && rawEventType === 'rename') {
+        eventType = 'fileRename';
+      } else if (info && info.type === 'dir' && rawEventType === 'change') {
+        eventType = 'dirChange';
+      } else if (info && info.type === 'dir' && rawEventType === 'rename') {
+        eventType = 'dirRename';
+      } else {
+        eventType = 'unknown';
       }
+
+      eventsBuffer.push({filename, eventType, info});
+
+      this.lastEventTimestamp = new Date().getTime();
+      const eventTimestamp = this.lastEventTimestamp;
+
+      new Promise(resolve => setTimeout(resolve, 100)).then(() => {
+        if (this.lastEventTimestamp === eventTimestamp) {
+          console.log(eventsBuffer.length);
+          const events = eventsBuffer;
+          eventsBuffer = [];
+          this.sendResponse(events);
+        }
+        return null;
+      }).catch(e => console.log(e));
+    });
+
+    return new Promise((resolve, reject) => {
+      this.resolveFunc = resolve;
+      this.rejectFunc = reject;
     });
   }
 }
