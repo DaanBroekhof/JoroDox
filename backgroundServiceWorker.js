049e497b87eba9da05ce4fd20f3650e97ebadc54
Daan Broekhof <daan.broekhof@gmail.com>
2023-07-12 19:54:28 +0200

    - Make extension icon clickable

--- backgroundServiceWorker.js
+++ backgroundServiceWorker.js
@@ -1,4 +1,8 @@
-chrome.windows.create({
-    url: 'index.html',
-    type: "popup"
-});
+chrome.action.onClicked.addListener(
+  () => {
+	  chrome.windows.create({
+	      url: 'index.html',
+	      type: "popup"
+	  });
+  }
+);
